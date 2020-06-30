a9daece7ce60b8ee850562d7cb652a0bd5db1634
Nafis Hasnain <nafishasnain0@gmail.com>
2020-06-29 22:08:28 -0400

    Add stateless function for Navbar

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react'
 import './Popular.css'
 
+const LanguageNavBar = ({ selected, onUpdateLanguage }) => {
+    const languages = ["All","JavaScript","Ruby","Java","CSS","Python"]
+    return (
+        <ul className="flex-center">
+            {languages.map(lang => (
+                <li key={lang}>
+                    <button className="btn-clear nav-link"
+                    style={lang === selected ? {color: 'rgb(187, 46, 31)'} : null}
+                    onClick={() => onUpdateLanguage(lang)}
+                    >
+                        {lang}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
 class Popular extends Component {
+    constructor(props){
+        super(props)
+
+        //state for language
+        this.state = {
+            selectedLanguage: 'All'
+        }
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+    //handler for updating language
+    updateLanguage(selectedLanguage){
+        this.setState({
+            selectedLanguage
+        })
+    }
+
     render() {
-        const languages = ["All","JavaScript","Ruby","Java","CSS","Python"]
+        const { selectedLanguage } = this.state
         return (
-            <ul className="flex-center">
-                {languages.map(lang => (
-                    <li key={lang}>
-                        <button className="btn-clear nav-link">
-                            {lang}
-                        </button>
-                    </li>
-                ))}
-            </ul>
+            <>
+                <LanguageNavBar 
+                    selected={selectedLanguage}
+                    onUpdateLanguage={this.updateLanguage}
+                />
+            </>
         )
     }
 }
