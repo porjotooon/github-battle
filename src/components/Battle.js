c30b8d3b4ad0791a4687a8ba718dc2154ddeccec
Nafis Hasnain <nafishasnain0@gmail.com>
2020-07-01 09:48:13 -0400

    Add Player component to update username state

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -23,6 +23,48 @@ const Instructions = () => {
     )
 }
 
+class Player extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            username: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(e){
+        this.setState({username: e.target.value})
+    }
+    handleSubmit(e){
+        e.preventDefault() // we don't want to have the normal browser events take place
+
+        this.props.onSubmit(this.state.username)
+    }
+
+    render(){
+        return (
+            <form className='column player' onSubmit={this.handleSubmit}>
+                <label htmlFor='username' className='player-label'>
+                    {this.props.label}
+                </label>
+                <div className='row player-inputs'>
+                    <input
+                      type='text'
+                      id='username'
+                      className='input-light'
+                      placeholder='github username'
+                      value={this.state.username}
+                      onChange={this.handleChange}
+                    />
+                    <button className='btn btn-dark' type='submit' disabled={!this.state.username}>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        )
+    }
+}
 export default class Battle extends Component {
     render(){
         return(
