c17ba3912327e9b02b86d08dd66fd168af459d1d
Nafis Hasnain <nafishasnain0@gmail.com>
2020-07-01 23:57:21 -0400

    Add id & secret id to api component

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,8 @@
+const id = "1f41f8cac4df61a8ac5b"
+const sec = "3661b3c1b7c0c5e0115370282e78f17162de3224"
+const params = `?client_id=${id}&client_secret=${sec}`
+
+
 export function fetchPopularRepos (language) {
     const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
