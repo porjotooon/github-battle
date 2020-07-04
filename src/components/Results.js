4ed512aed6926d51ee852bbd55945a50e60e7c7f
Nafis Hasnain <nafishasnain0@gmail.com>
2020-07-04 08:57:57 -0400

    Add business logic for the battle

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
+import { battle } from '../utils/api'
 
 export default class Results extends Component {
+    componentDidMount(){
+        const { playerOne, playerTwo }= this.props
+
+        battle([ playerOne, playerTwo ])
+        .then(players => {
+            
+        })
+    }
+
     render(){
         return(
             <div>
