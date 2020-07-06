d20567d9d54b66e2cb9423f7f58e56a5f6797c35
Nafis Hasnain <nafishasnain0@gmail.com>
2020-07-06 10:05:54 -0400

    Add navbar to switch between Popular & Battle

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,20 @@
-
 import React from 'react'
 import { ThemeConsumer } from '../context/theme'
+import {NavLink} from 'react-router-dom'
 
 export default function Nav () {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className='row space-between'>
+          <ul className='row nav'>
+            <li>
+              <NavLink activeStyle={{color: 'rgb(187,46,31)'}} to='/' exact className='nav-link'>Popular</NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={{color: 'rgb(187,46,31)'}} to='/battle' className='nav-link'>Battle</NavLink>
+            </li>
+          </ul>
           <button
             style={{fontSize: 30}}
             className='btn-clear'
